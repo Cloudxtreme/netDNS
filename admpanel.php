836b92b615974f5bdad1204c917c48834b12654c
Ted Lin <g3658367@gmail.com>
2013-07-08 23:09:21 +0800

    Admin Panel --> API
    Signed-off-by: Ted Lin <g3658367@gmail.com>

--- admpanel.php
+++ admpanel.php
@@ -1,116 +1,9 @@
 <?php
+	require('dmapi.php');
+	require('db.php');
 	session_start();
-	if($_SESSION['auth']!=true)
-	{
-		header('Location: permissiondeny.php');
-		exit;
-	}
-	if($_SESSION['admin']!=true)
-	{
-		header('Location: permissiondeny.php');
-		exit;
-	}
-	try {
-		include('db.php');
-		
-		$pubdomain=$db->query('SELECT * FROM pub_domain');
-		$pubdomain_arr=$pubdomain->fetchAll();
-		
-		if(isset($_POST['pub_host']))
-		{
-			$hostnamecheck=$db->query('SELECT * FROM user_list WHERE hostname =\''.$_POST["pub_host"].'\'');
-			$hostnamecheck_arr=$hostnamecheck->fetchAll();
-			$pubhostnamecheck=$db->query("SELECT * FROM pub_domain WHERE hostname ='".$_POST['pub_host']."'");
-			$pubhostnamecheck_arr=$pubhostnamecheck->fetchAll();
-			/*Check if the domain is exist in public domain */
-			if(count($pubhostnamecheck_arr)==0)
-			{
-				/*Check if the hostname is registered or not*/
-				if(count($hostnamecheck_arr)==0)
-				{
-					// Prepare INSERT statement to db
-					$insert = "INSERT INTO pub_domain (creator,type,hostname,ip)
-									VALUES (:name,:type,:hostname,:ip)";
-									
-					$stmt = $db->prepare ($insert);
-					
-						//Bind parameter to variable
-						$stmt->bindParam (':name'		, $_POST['pub_creator'] );
-						$stmt->bindParam (':type'		, $_POST['pub_type'] );
-						$stmt->bindParam (':hostname' 	, $_POST['pub_host'] );
-						$stmt->bindParam (':ip' 		, $_POST['pub_ip'] );
-							
-						// Execute statement
-						if ($stmt->execute() == FALSE) {
-		?>
-						<script>
-							window.alert ("Add Domain ERROR!");
-						</script>
-<?php
-						}
-						$rundomain=1;
-				} else {
-					$err2 = "The hostname was already registered.";
-				}
-			} else {
-				$err2 = "The hostname was already registered.";
-			}
-		}
-		
-		if(isset($_POST['name']))
-		{
-			$usercheck=$db->query('SELECT * FROM user_list WHERE name =\''.$_POST["name"].'\'');
-			$usercheck_arr=$usercheck->fetchAll();
-			$hostnamecheck=$db->query('SELECT * FROM user_list WHERE hostname =\''.$_POST["hostname"].'\'');
-			$hostnamecheck_arr=$hostnamecheck->fetchAll();
-			
-			/*Check if the user is exist or not */
-			if(count($usercheck_arr)==0)
-			{
-				/*Check if the hostname is registered or not*/
-				if(count($hostnamecheck_arr)==0)
-				{
-					// Prepare INSERT statement to db
-					$insert = "INSERT INTO user_list (name,password,hostname,fullname)
-									VALUES (:name,:password,:hostname,:fullname)";
-									
-					$stmt = $db->prepare ($insert);
-					
-						//Bind parameter to variable
-						$stmt->bindParam (':name'		, $_POST['name'] );
-						$stmt->bindParam (':password'   , md5($_POST['password']) );
-						if(isset($_POST['hostname']))
-							$stmt->bindParam (':hostname' , $_POST['hostname'] );
-						else
-							$stmt->bindParam (':hostname' , '');
-						$stmt->bindParam (':fullname'		, $_POST['fullname'] );
-							
-						// Execute statement
-						if ($stmt->execute() == FALSE) {
-		?>
-						<script>
-							window.alert ("Create Account ERROR!");
-						</script>
-<?php
-						}
-				} else {
-					$err = "The hostname was already registered.";
-				}
-			} else {
-				$err = "The user was already exist.";
-			}
-		}
-		$acc_list=$db->query("SELECT * FROM user_list");
-		$acc_list_arr=$acc_list->fetchAll();
-		
-		$db = null;
-	}
-	catch (PDOException $e)
-    {
-	    echo 'DB operation failed!<br>' . $e->getMessage () . '<br>';
-	    $db = null;
-    }
-	
+	permissionCheck($_SESSION['auth']);
+	permissionCheck($_SESSION['admin']);
 ?>
 <!doctype html>
 <html>
@@ -130,90 +23,47 @@
     <p style="text-align:right; padding-right:50px;color:#F5F5F5;">Login:&nbsp;<?php echo $_SESSION['user'];?>&nbsp;&nbsp;<a href="udomain.php">[Personal Domain]</a>&nbsp;<a href="logout.php">[Logout]</a></p>
 	<h1 style="text-align:center;color:#F5F5F5;">DNS Management Panel</h1>
     <h4 style="text-align:center;color:#F5F5F5;">net.nsysu.edu.tw</h4>
-    <?php /*======================== Public Domain Block ========================*/ ?>
-    <div class="main" style="float:right;">
-        <h1 style="text-align:center;">Public Domain</h1>
-        
-        <form method="post" id="upddom" action="upddom.php">
-        <table class="table table-bordered">
-            <tr>
-                <td>ID</td>
-                <td>Creator</td>
-                <td>Type</td>
-                <td>Hostname</td>
-                <td>IP</td>
-                <td>Update</td>
-                <td>Delete</td>
-            </tr>
-    <?php foreach($pubdomain_arr as $row) { ?>
-            <tr>
-                <td style="text-align:center;"><?php echo $row['id']; ?><input name="id" type="hidden" value="<?php echo $row['id']; ?>"></td>
-                <td style="text-align:center;"><?php echo $row['creator']; ?></td>
-                <td style="text-align:center;"><?php echo $row['type']; ?></td>
-                <td style="text-align:center;"><?php echo $row['hostname']; ?></td>         
-                <td style="text-align:center;"><input style="width:110px" name="ip" type="text" value="<?php echo $row['ip']; ?>"></td>
-                <td style="text-align:center;"><input class="btn btn-warning" type="submit" value="Go!" onclick="if(confirm('Do you really want to update with new IP?')) return true;else return false"></td>
-                <td style="text-align:center;"><a href="deldom.php?id=<?php echo $row['id']; ?>"><img width="30" src="img/del.png" onclick="if(confirm('Do you really want to remove this domain?')) return true;else return false"></a></td>
-            </tr>
-    <?php } ?>
-        </table>
-        </form>
-        <h4 style="text-align:center;">Add one immediately</h4>
-        <form class="regi" method="post" id="pubdomain">
-        	<input name="pub_type" type="hidden" value="A">
-            <table class="table">
-            	<caption>A record</caption>
-                <tr>
-                    <td style="text-align:right;">Hostname</td>
-                    <td><input style="width:100px;" type="text" name="pub_host" placeholder="Domain name" required>.net.nsysu.edu.tw</td>
-                </tr>
-                <tr>
-                    <td style="text-align:right;">IP</td>
-                    <td><input style="width:110px;" type="text" name="pub_ip" placeholder="IP Address" value="<?php //echo $_SERVER['REMOTE_ADDR'];?>" required></td>
-                </tr>
-                <tr><td colspan="2" style="text-align:center;"><input name="pub_creator" type="hidden" value="<?php echo $_SESSION['user']; ?>"><button class="btn btn-primary" type="submit">Add</button></td></tr>
-            </table>
-        </form>
+    <p>&nbsp;</p>
+    <?php /*======================== Action Log Block (DB action)========================*/ ?>
+    <div class="main" style="height:300px;float:right; position:relative; top:840px; overflow:auto;">
+        <h1 style="text-align:center;">Action Log</h1>
         <?php
-        if(file_exists("/tmp/tmp_nsupdate_".$_SESSION['user']))
-        {
-            if(unlink("/tmp/tmp_nsupdate_".$_SESSION['user']))
-                $hint1 = "Delete tmp file.<br>";
-            else
-                $err1 = "Delete tmp file error!(file not found?)<br>";
-        }
-        if(isset($hint1)) { ?><p style="text-align:center; color:#3C0;"><img width="50" src="img/good.png"> &nbsp; <?php echo $hint1; ?></p><?php } 
-        if(isset($err1)) { ?><p style="text-align:center; color:#F00;"><img width="50" src="img/sad.png"> &nbsp; <?php echo $err1; ?></p><?php }
+			clearActionTmp($_SESSION['user']);
+			try {
+				$pubdomain=$db->query('SELECT * FROM pub_domain');
+				$pubdomain_arr=$pubdomain->fetchAll();
+				
+				if(isset($_POST['pub_host']))
+				{
+					addPublicDomain_DB($_POST['pub_creator'],$_POST['pub_type'],$_POST['pub_host'],$_POST['pub_ip']);
+				}
+				
+				if(isset($_POST['name']))
+				{
+					addUser($_POST['name'],$_POST['password'],$_POST['hostname'],$_POST['fullname']);
+				}
+				
+				$acc_list=$db->query("SELECT * FROM user_list");
+				$acc_list_arr=$acc_list->fetchAll();
+				$pd_list=$db->query("SELECT * FROM pub_domain");
+				$pd_list_arr=$pd_list->fetchAll();
+				
+				$db = null;
+			}
+			catch (PDOException $e)
+			{
+				$err = 'Database operation failed!<br>' . $e->getMessage () . '<br>';
+				$db = null;
+			}
+			if(isset($err)) { ?><p style="text-align:center; color:#F00;"><img width="50" src="img/sad.png"> &nbsp; <?php echo $err; ?></p><?php }
+			
+			createUpdateTmp($_SESSION['user'],$new_pd_arr);
         
-        if($rundomain==1)
-        {		
-            /*Renew DNS List */
-            $file=fopen("/tmp/tmp_nsupdate_".$_SESSION['user'],"w");
-            fprintf($file,"server net.nsysu.edu.tw\n");
-            fprintf($file,"zone net.nsysu.edu.tw\n");
-            fprintf($file,"update delete %s.net.nsysu.edu.tw\n",$_POST['pub_host']);
-            fprintf($file,"update add %s.net.nsysu.edu.tw 604800 A %s\n",$_POST["pub_host"],$_POST["pub_ip"]);
-            fprintf($file,"send\n");
-            $hint2 = "Script file created.";
-        }
-        if(isset($err2)) { ?><p style="text-align:center; color:#F00;"><img width="50" src="img/sad.png"> &nbsp; <?php echo $err2; ?></p><?php }
-        if(isset($hint2)) { ?><p style="text-align:center; color:#3C0;"><?php echo $hint2; ?></p><?php }
-        
-        if(file_exists("/tmp/tmp_nsupdate_".$_SESSION['user']))
-        {
-            $output=nl2br(shell_exec("/usr/bin/sudo /usr/bin/nsupdate -d -k /etc/bind/Knet.nsysu.+157+55142.key /tmp/tmp_nsupdate_".$_SESSION['user']));
-            if($output)
-            {
-                //echo "<li>".$output."</li>";
-                $hint3 = "Domain created.";
-            } else {
-                $err3 = "Something failed?...";
-            }
-        }
-        if(isset($hint3)) { ?><p style="text-align:center; color:#3C0;"><img width="50" src="img/good.png"> &nbsp; <?php echo $hint3; ?></p><?php }
-        if(isset($err3)) { ?><p style="text-align:center; color:#F00;"><img width="50" src="img/sad.png"> &nbsp; <?php echo $err3; ?></p><?php }
-    ?>
+			execDNSaction($_SESSION['user'],"created");
+		?>
     </div>
+    
+    
     <?php /*======================== Create Account Block ========================*/ ?>
     <div class="main" style="float:left;">
         <h1 style="text-align:center;">Create Account</h1>
@@ -242,8 +92,59 @@
                 </tr>
             </table>
         </form>
-        <?php if(isset($err)) { ?><p style="text-align:center; color:#F00;"><img width="50" src="img/sad.png"> &nbsp; <?php echo $err; ?></p><?php } ?>
+        <?php /*if(isset($err)) { ?><p style="text-align:center; color:#F00;"><img width="50" src="img/sad.png"> &nbsp; <?php echo $err; ?></p><?php }*/ ?>
     </div>
+    
+    <?php /*======================== Public Domain Block ========================*/ ?>
+    <div class="main" style="height:780px;float:right; position:relative; top:-350px; overflow:auto;">
+        <h1 style="text-align:center;">Public Domain</h1>
+        
+        <form method="post" id="upddom" action="upddom.php">
+        <table class="table table-bordered">
+            <tr>
+                <td>ID</td>
+                <td>Creator</td>
+                <td>Type</td>
+                <td>Hostname</td>
+                <td>IP</td>
+                <td>Update</td>
+                <td>Delete</td>
+            </tr>
+    <?php foreach($pd_list_arr as $row) { ?>
+            <tr>
+                <td style="text-align:center;"><?php echo $row['id']; ?><input name="id" type="hidden" value="<?php echo $row['id']; ?>"></td>
+                <td style="text-align:center;"><?php echo $row['creator']; ?></td>
+                <td style="text-align:center;"><?php echo $row['type']; ?></td>
+                <td style="text-align:center;"><?php echo $row['hostname']; ?></td>         
+                <td style="text-align:center;"><input style="width:110px" name="ip" type="text" value="<?php echo $row['ip']; ?>"></td>
+                <td style="text-align:center;"><input class="btn btn-warning" type="submit" value="Go!" onclick="if(confirm('Do you really want to update with new IP?')) return true;else return false"></td>
+                <td style="text-align:center;"><a href="deldom.php?id=<?php echo $row['id']; ?>"><img width="30" src="img/del.png" onclick="if(confirm('Do you really want to remove this domain?')) return true;else return false"></a></td>
+            </tr>
+    <?php } ?>
+        </table>
+        </form>
+        <h4 style="text-align:center;">Add one immediately</h4>
+        <form class="regi" method="post" id="pubdomain">
+        	<input name="pub_type" type="hidden" value="A">
+            <table class="table">
+            	<caption>A & CNAME record only</caption>
+                <tr>
+                    <td style="text-align:right;">Hostname</td>
+                    <td><input style="width:100px;" type="text" name="pub_host" placeholder="Domain name" required>.net.nsysu.edu.tw</td>
+                </tr>
+             	<tr>
+                    <td style="text-align:right;">Type</td>
+                    <td><select style="width:45px;" name="pub_type" size="1"><option>A</option><option>C</option></select>&nbsp;&nbsp;(A = A Record, C = CNAME Record)</td>
+                </tr>
+                <tr>
+                    <td style="text-align:right;">IP/Alias</td>
+                    <td><input style="width:120px;" type="text" name="pub_ip" placeholder="IP Address or Alias" value="<?php //echo $_SERVER['REMOTE_ADDR'];?>" required></td>
+                </tr>
+                <tr><td colspan="2" style="text-align:center;"><input name="pub_creator" type="hidden" value="<?php echo $_SESSION['user']; ?>"><button class="btn btn-primary" type="submit">Add</button></td></tr>
+            </table>
+        </form>
+    </div>
+    
     <?php /*======================== Management Account Block ========================*/ ?>
     <div class="main" style="float:left;">
         <h1 style="text-align:center;">Account Management</h1>
@@ -280,6 +181,7 @@
     <?php } ?>
         </table>
     </div>
+    
 </div>
 
 <?php /*
--- dmapi.php
+++ dmapi.php
@@ -9,7 +9,7 @@ function permissionCheck($session)
 	}
 }
 
-function addPublicDomain_DB($creator,$type,$host,$ip) //Pass domain information
+function doubleDomainCheck($host)
 {
 	$hostnamecheck=$GLOBALS['db']->query('SELECT * FROM user_list WHERE hostname =\''.$host.'\'');
 	$hostnamecheck_arr=$hostnamecheck->fetchAll();
@@ -21,33 +21,84 @@ function addPublicDomain_DB($creator,$type,$host,$ip) //Pass domain information
 		/*Check if the hostname is registered or not*/
 		if(count($hostnamecheck_arr)==0)
 		{
-			// Prepare INSERT statement to db
-			$insert = "INSERT INTO pub_domain (creator,type,hostname,ip)
-							VALUES (:name,:type,:hostname,:ip)";
-							
-			$stmt = $GLOBALS['db']->prepare ($insert);
-			
-				//Bind parameter to variable
-				$stmt->bindParam (':name'		, $creator );
-				$stmt->bindParam (':type'		, $type );
-				$stmt->bindParam (':hostname' 	, $host );
-				$stmt->bindParam (':ip' 		, $ip );
-					
-				// Execute statement
-				if ($stmt->execute() == FALSE) {
-?>
-				<script>
-					window.alert ("Add Domain ERROR!");
-				</script>
-<?php
-				}
-				$rundomain=1;
-		} else {
-			$err = "The hostname was already registered.";
+			return 1;
 		}
+	}
+}
+
+function addUser($name,$pass,$host,$fullname)
+{
+	$usercheck=$GLOBALS['db']->query('SELECT * FROM user_list WHERE name =\''.$name.'\'');
+	$usercheck_arr=$usercheck->fetchAll();
+	/*Check if the user is exist or not */
+			if(count($usercheck_arr)==0)
+			{
+				/*Check if the hostname is registered or not*/
+				if(doubleDomainCheck($host))
+				{
+					// Prepare INSERT statement to db
+					$insert = "INSERT INTO user_list (name,password,hostname,fullname)
+									VALUES (:name,:password,:hostname,:fullname)";
+									
+					$stmt = $GLOBALS['db']->prepare ($insert);
+					
+						//Bind parameter to variable
+						$stmt->bindParam (':name'		, $name );
+						$password=md5($pass);
+						$stmt->bindParam (':password'   , $password );
+						if(strlen($host)!=0 && $host!=NULL) {
+							$stmt->bindParam (':hostname' , $host );
+						} else {
+							$host="";
+							$stmt->bindParam (':hostname' , $host );
+						}
+						$stmt->bindParam (':fullname'		, $fullname );
+							
+						// Execute statement
+						if ($stmt->execute() == FALSE) {
+							$err = "Create account failed.";
+						}
+				} else {
+					$err = "The hostname was already registered.";
+				}
+			} else {
+				$err = "The user was already exist.";
+			}
+}
+
+function addPublicDomain_DB($creator,$type,$host,$ip) //Pass domain information
+{
+	//Check if the hostname is registered or not
+	if(doubleDomainCheck($host))
+	{
+		// Prepare INSERT statement to db
+		$insert = "INSERT INTO pub_domain (creator,type,hostname,ip)
+						VALUES (:name,:type,:hostname,:ip)";
+						
+		$stmt = $GLOBALS['db']->prepare ($insert);
+		
+			//Bind parameter to variable
+			$stmt->bindParam (':name'		, $creator );
+			$stmt->bindParam (':type'		, $type );
+			$stmt->bindParam (':hostname' 	, $host );
+			$stmt->bindParam (':ip' 		, $ip );
+				
+			// Execute statement
+			if ($stmt->execute() == FALSE) {
+				$err = "Add domain error.";
+			} else {
+				$hint="Add domain successful!";
+				global $runaction;
+				$runaction=1;
+			}
 	} else {
 		$err = "The hostname was already registered.";
 	}
+	$new_pd=$GLOBALS['db']->query('SELECT * FROM pub_domain WHERE hostname =\''.$host.'\'');
+	global $new_pd_arr;
+	$new_pd_arr=$new_pd->fetchAll();
+	if(isset($hint)) { ?><p style="text-align:center; color:#3C0;"><img width="50" src="img/good.png"> &nbsp; <?php echo $hint; ?></p><?php } 
+	if(isset($err)) { ?><p style="text-align:center; color:#F00;"><img width="50" src="img/sad.png"> &nbsp; <?php echo $err; ?></p><?php }
 }
 
 function delPublicDomain_DB($id) //Pass domain ID
@@ -81,6 +132,21 @@ function delPublicDomain_DB($id) //Pass domain ID
 	if(isset($err)) { ?><p style="text-align:center; color:#F00;"><img width="50" src="img/sad.png"> &nbsp; <?php echo $err; ?></p><?php }
 }
 
+function updatePublicDomain_DB($id,$ip)
+{
+	$update = "UPDATE pub_domain SET ip = :ip WHERE id = :id";
+	$stmt = $GLOBALS['db']->prepare($update);
+			
+	$stmt->bindParam(':ip', $ip);
+	$stmt->bindParam(':id', $id);
+	
+	if($stmt->execute()==FALSE) {
+		$err="Something wrong with PDO operation. =(";
+	}
+	global $runaction;
+	$runaction=1;
+}
+
 function clearActionTmp($user)
 {
 	if(file_exists("/tmp/tmp_nsupdate_".$user))
@@ -99,12 +165,27 @@ function createDelTmp($user,$data)
 	global $runaction;
 	if($runaction==1)
     {
-        $file=fopen("/tmp/tmp_nsupdate_".$_SESSION['user'],"w");
+        $file=fopen("/tmp/tmp_nsupdate_".$user,"w");
         fprintf($file,"server net.nsysu.edu.tw\n");
         fprintf($file,"zone net.nsysu.edu.tw\n");
         fprintf($file,"update delete %s.net.nsysu.edu.tw\n",$data[0]['hostname']);
         fprintf($file,"send\n");
-        $hint = "Script file created.";
+        $hint = "Delete script file created.";
+    }
+	if(isset($hint)) { ?><p style="text-align:center; color:#3C0;"><?php echo $hint; ?></p><?php }
+}
+
+function createUpdateTmp($user,$data)
+{
+	if($GLOBALS['runaction']==1)
+    {
+        $file=fopen("/tmp/tmp_nsupdate_".$user,"w");
+        fprintf($file,"server net.nsysu.edu.tw\n");
+        fprintf($file,"zone net.nsysu.edu.tw\n");
+        fprintf($file,"update delete %s.net.nsysu.edu.tw\n",$data[0]['hostname']);
+		fprintf($file,"update add %s.net.nsysu.edu.tw 604800 A %s\n",$data[0]['hostname'],$data[0]['ip']);
+        fprintf($file,"send\n");
+        $hint = "Update script file created.";
     }
 	if(isset($hint)) { ?><p style="text-align:center; color:#3C0;"><?php echo $hint; ?></p><?php }
 }
@@ -119,7 +200,7 @@ function execDNSaction($user,$action) //Pass $_SESSION['user'] & action string
             //echo "<li>".$output."</li>";
             $hint = "Domain ".$action.".";
         } else {
-            $err = "Something failed...";
+            $err = "Something failed...(Key not found? Exec permission?)";
         }
     }
 	if(isset($hint)) { ?><p style="text-align:center; color:#3C0;"><img width="50" src="img/good.png"> &nbsp; <?php echo $hint; ?></p><?php }
